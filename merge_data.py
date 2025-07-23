df08100e5501804520e364f29eb47a3811b8cb5a
wangzhuoyang <wangzhuoyang@ciictec.com>
2025-07-23 22:22:40 +0800

    删除无关代码

--- merge_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_excel('/Users/wangzhuoyang/dalian/历史数据 (2).xlsx')
-df['total_flow'] = (df['DLDZ_AVS_LLJ01_FI01.PV'] + df['DLDZ_DQ200_LLJ01_FI01.PV'])/2
-df.drop(columns=['DLDZ_AVS_LLJ01_FI01.PV', 'DLDZ_DQ200_LLJ01_FI01.PV'], inplace=True)
-df.rename(columns={'时间': 'time'}, inplace=True)
-df.to_csv('/Users/wangzhuoyang/dalian/历史数据3_total_flow.csv', index=False)
